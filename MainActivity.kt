7bd87f64eeac37db36a473d3fe717962d50ba7ae
patrassingh91152-sudo <patrassingh91152@gmail.com>
2026-04-30 19:05:04 +0530

    Create MainActivity.kt

---
+++ MainActivity.kt
@@ -0,0 +1,28 @@
+plugins {
+    id "com.android.application"
+    id "org.jetbrains.kotlin.android"
+    id "dev.flutter.flutter-gradle-plugin"
+}
+
+android {
+    namespace "com.patras.aviatorinsightspro"
+    compileSdk 34
+
+    defaultConfig {
+        applicationId "com.patras.aviatorinsightspro"
+        minSdk 21
+        targetSdk 34
+        versionCode 1
+        versionName "1.0"
+    }
+
+    buildTypes {
+        release {
+            signingConfig signingConfigs.debug
+        }
+    }
+}
+
+flutter {
+    source '../..'
+} 
